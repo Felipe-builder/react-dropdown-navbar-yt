340723a94bf677ff4d9e83f0bbb21f01e5b24b5b
Felipe Vasconcelos <felipesvascon@gmail.com>
2023-04-22 19:34:50 -0300

    Studing react

---
+++ src/components/pages/Marketing.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+export default function Marketing() {
+  return (
+    <>
+      <h1 className='consulting'>
+        MARKETING
+      </h1>
+    </>
+  )
+}
---
+++ src/components/pages/Services.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+export default function Services() {
+  return (
+    <>
+      <h1 className='consulting'>
+        SERVICES
+      </h1>
+    </>
+  )
+}
